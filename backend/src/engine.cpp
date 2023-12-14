2ef45c09ca014827841e1313a692c1d4b83fdd86
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2023-12-14 07:44:22 +0000

    支持高权限免密码

--- backend/src/engine.cpp
+++ backend/src/engine.cpp
@@ -122,11 +122,12 @@ std::vector<std::string> BookStoreEngineClass::ExecuteSu(
   if (!CommandSuLexer(cmd, user_id, password))
     return std::vector<std::string>({"Invalid"});
   // debugPrint("su", user_id, " ", password);
+  if (user_data_base.GetPrevilege(user_id) == -1)
+    return std::vector<std::string>({"Invalid"});
   if (login_stack.size() > 0 &&
-      user_data_base.GetPrevilege(login_stack.top().first) == 7) {
+      user_data_base.GetPrevilege(login_stack.top().first) >
+          user_data_base.GetPrevilege(user_id)) {
     // debugPrint("has root previlege");
-    if (user_data_base.GetPrevilege(user_id) == -1)
-      return std::vector<std::string>({"Invalid"});
     login_stack.push(std::make_pair(user_id, 0));
     login_count[user_id]++;
     return std::vector<std::string>();
