f1c95ea9875cf46be6cc76561a33087519d598d7
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2023-12-16 08:40:03 +0000

    完成注册页面

--- frontend/Web/index.js
+++ frontend/Web/index.js
@@ -158,6 +158,10 @@ app.get('/login', (req, res) => {
   res.sendFile(join(__dirname, 'login.html'));
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(join(__dirname, 'register.html'));
+});
+
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
