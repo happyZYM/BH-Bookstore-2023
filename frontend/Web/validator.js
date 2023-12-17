8574b188f8871f6c2d8ef177b06bcf5dcb1df67e
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2023-12-17 03:16:07 +0000

    修复了无法普通模式启动的bug

--- frontend/Web/validator.js
+++ frontend/Web/validator.js
@@ -5,24 +5,27 @@ console.log(process.env['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
 
 
   // 注意，此处实例化的client请尽可能重复使用，避免重复建立连接，提升检测性能。
-let client = new RPCClient({
-    /**
-* 阿里云账号AccessKey拥有所有API的访问权限，建议您使用RAM用户进行API访问或日常运维。
-* 强烈建议不要把AccessKey ID和AccessKey Secret保存到工程代码里，否则可能导致AccessKey泄露，威胁您账号下所有资源的安全。
-* 常见获取环境变量方式：
-* 获取RAM用户AccessKey ID：process.env['ALIBABA_CLOUD_ACCESS_KEY_ID']
-* 获取RAM用户AccessKey Secret：process.env['ALIBABA_CLOUD_ACCESS_KEY_SECRET']
-*/
-    accessKeyId: process.env['ALIBABA_CLOUD_ACCESS_KEY_ID'],
-    accessKeySecret: process.env['ALIBABA_CLOUD_ACCESS_KEY_SECRET'],
-    // 接入区域和地址请根据实际情况修改
-    endpoint: "https://green-cip.cn-beijing.aliyuncs.com",
-    apiVersion: '2022-03-02',
-    // 设置http代理
-    // httpProxy: "http://xx.xx.xx.xx:xxxx",
-    // 设置https代理
-    // httpsProxy: "https://username:password@xxx.xxx.xxx.xxx:9999",
-});
+let client;
+if(process.env['VALIDING']=='True'){
+  client=new RPCClient({
+      /**
+  * 阿里云账号AccessKey拥有所有API的访问权限，建议您使用RAM用户进行API访问或日常运维。
+  * 强烈建议不要把AccessKey ID和AccessKey Secret保存到工程代码里，否则可能导致AccessKey泄露，威胁您账号下所有资源的安全。
+  * 常见获取环境变量方式：
+  * 获取RAM用户AccessKey ID：process.env['ALIBABA_CLOUD_ACCESS_KEY_ID']
+  * 获取RAM用户AccessKey Secret：process.env['ALIBABA_CLOUD_ACCESS_KEY_SECRET']
+  */
+      accessKeyId: process.env['ALIBABA_CLOUD_ACCESS_KEY_ID'],
+      accessKeySecret: process.env['ALIBABA_CLOUD_ACCESS_KEY_SECRET'],
+      // 接入区域和地址请根据实际情况修改
+      endpoint: "https://green-cip.cn-beijing.aliyuncs.com",
+      apiVersion: '2022-03-02',
+      // 设置http代理
+      // httpProxy: "http://xx.xx.xx.xx:xxxx",
+      // 设置https代理
+      // httpsProxy: "https://username:password@xxx.xxx.xxx.xxx:9999",
+  });
+}
 async function IsValid(text) {
   console.log("IsValid called");
   console.log("cheching text: "+text);
