b31e74d0d39a5aa20e9e6789aa11324a5241fc63
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2023-12-18 06:57:24 +0000

    手动同步了客户端中的更改

--- frontend/client/main.js
+++ frontend/client/main.js
@@ -1,8 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  const win = new BrowserWindow();
-  win.loadURL('http://localhost:4000/');
+  const win = new BrowserWindow({
+    fullscreen: true, // 设置默认全屏
+    frame: false, // 隐藏导航栏
+  });
+
+  win.loadURL('http://bookstore.zymsite.ink/');
+
+  // 监听按键事件
+  win.webContents.on('before-input-event', (event, input) => {
+    if (input.key === 'Escape') {
+      app.quit(); // 按下Esc键时退出程序
+    }
+  });
 }
 
 app.whenReady().then(createWindow);
